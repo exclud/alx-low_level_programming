b767a2abf9e93629ef6cf809f06db5841776769a
exclud <shammahnjomo12@gmail.com>
2023-02-16 02:02:02 -0800

    Added sizeof and the put function[F

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *              followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints the string "with proper grammar, but the outcome is a piece of art,"
+ *              followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
+
