ca6a46a6de18e51fe526eacb3afae81f0a846979
exclud <shammahnjomo12@gmail.com>
2023-02-16 04:05:10 -0800

    Fixed Betty Coding Style Character Error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints the string "with proper grammar, but the outcome is a piece of art,"
- *              followed by a new line.
+ * Description: Prints the string "with proper grammar, but the outcome is a
+ *              piece of art," followed by a new line.
  *
  * Return: Always 0 (success)
  */
