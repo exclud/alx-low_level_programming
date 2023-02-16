89e25f73e7e57806110095ba34ff9c8188dcde45
exclud <shammahnjomo12@gmail.com>
2023-02-16 12:16:04 -0800

    Fixed Intel Syntax error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints the string "with proper grammar, but the outcome is a piece of art,"
+ *              followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+#include <unistd.h>
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
+
 
