b7c7a768b7c5d56953f0457f88d27e30bedebae2
exclud <shammahnjomo12@gmail.com>
2023-02-21 05:00:48 -0800

    Betty Style coding update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
  *
- * Return: Always 0.
+ * Return: void
  */
 void print_alphabet(void)
 {
 int i;
-for(i=0;i<26;i++)
+for (i = 0 ; i < 26 ; i++)
 {
-_putchar(i+'a');
+_putchar (i + 'a');
 }
-_putchar('\n');
+_putchar ('\n');
 }
