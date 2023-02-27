1c7c817e00df200f30f79fe8ed53ba08f5128bea
exclud <shammahnjomo12@gmail.com>
2023-02-27 10:39:13 -0800

    Added new header files

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,16 @@
+julien@ubuntu:~/0x05$ cat 4-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    print_rev(str);
+    return (0);
+}
