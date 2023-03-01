c95b3077a3ab85042f65d41e995296ef426a21b5
exclud <shammahnjomo12@gmail.com>
2023-02-28 23:14:28 -0800

    Added all files necessary to the directory

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*putchar function prototype*/
+int _putchar(char c);
+
+/* function prototype*/
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+
+#endif /*MAIN_H*/
