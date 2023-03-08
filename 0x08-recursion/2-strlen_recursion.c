bc4f6c31b3dd888f76f629e56c1bf404aefdd9c1
exclud <shammahnjomo12@gmail.com>
2023-03-08 11:52:50 -0800

    Fixed undefined reference issue

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: Pointer to the string to be measured.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
+}
+
