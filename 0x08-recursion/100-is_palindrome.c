430d2af6079df7237b957cbfa9583f37be066061
exclud <shammahnjomo12@gmail.com>
2023-03-08 12:10:54 -0800

    Fixed Loop Issue

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,24 @@
 /**
+ * _is_palindrome - Recursively checks if a given string is a palindrome.
+ *
+ * @s: The string to check.
+ * @start: The starting index of the current substring being checked.
+ * @end: The ending index of the current substring being checked.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int _is_palindrome(char *s, int start, int end)
+{
+if (start >= end)
+return (1);
+
+if (s[start] != s[end])
+return (0);
+
+return (_is_palindrome(s, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - Checks if a given string is a palindrome.
  *
  * @s: The string to check.
@@ -8,17 +28,10 @@
 int is_palindrome(char *s)
 {
 int length = 0;
-int i;
 
 while (s[length] != '\0')
 length++;
 
-for (i = 0; i < length / 2; i++)
-{
-if (s[i] != s[length - i - 1])
-return (0);
-}
-
-return (1);
+return (_is_palindrome(s, 0, length - 1));
 }
 
