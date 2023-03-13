59ca27b68ba124881f388f5847bc30938587a8a0
exclud <shammahnjomo12@gmail.com>
2023-03-12 22:38:40 -0700

    Completed Setup

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H /
+
+/*putchar function prototype*/
+int _putchar(char c);
+
+/* function prototype*/
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif /*MAIN_H*/
