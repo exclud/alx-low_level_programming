ba218255112c1ad7a296933dc2adc07550344f21
exclud <shammahnjomo12@gmail.com>
2023-03-20 15:17:09 -0700

    File Namewq

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/**
+ * main - Prints the name of the current source file
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+std::cout << __FILE__ << std::endl;
+return (0);
+}
+
