5ca01a7b44173a39edd88c29fd6acbe8fa2c9aa1
exclud <shammahnjomo12@gmail.com>
2023-04-10 18:47:12 +0300

    a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,12 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /*putchar function prototype*/
 int _putchar(char c);
 
 /* function prototype*/
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
