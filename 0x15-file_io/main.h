5f22d9a5f4e859f16079d90d0193e3e5111f463d
exclud <shammahnjomo12@gmail.com>
2023-04-10 18:40:36 +0300

    Added Function Prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H /
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /*putchar function prototype*/
 int _putchar(char c);
 
 /* function prototype*/
+size_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 
 #endif /*MAIN_H*/
