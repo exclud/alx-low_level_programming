c851dda284843aad8fc7494f1bb9c54b876ea770
exclud <shammahnjomo12@gmail.com>
2023-04-10 18:55:11 +0300

    a program that copies the content of a file to another file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 /*putchar function prototype*/
 int _putchar(char c);
@@ -14,6 +15,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char **argv);
 
 
 #endif /*MAIN_H*/
