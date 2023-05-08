62edce28ab8f75eb09df0feebbc1d29953784d4b
exclud <shammahnjomo12@gmail.com>
2023-05-08 11:27:13 +0300

    C- File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to the file name
+ * @letters: number of letters to read and print
+ * Return: actual number of letters read and printed, or 0 on failure
+ **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t n_read, n_written;
+char *buffer;
+int fd;
+
+if (filename == NULL)
+return (0);
+
+buffer = malloc(sizeof(char) * (letters + 1));
+if (buffer == NULL)
+return (0);
+
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+free(buffer);
+return (0);
+}
+
+n_read = read(fd, buffer, letters);
+if (n_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+n_written = write(STDOUT_FILENO, buffer, n_read);
+if (n_written == -1 || n_written != n_read)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+
+free(buffer);
+close(fd);
+
+return (n_written);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file with the given name and writes the text content
+ *               into it.
+ *
+ * @filename: The name of the file to be created.
+ * @text_content: The text content to be written into the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd, w_len = 0;
+
+if (filename == NULL)
+return (-1);
+
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
+
+if (text_content != NULL)
+{
+while (text_content[w_len])
+w_len++;
+
+if (write(fd, text_content, w_len) == -1)
+return (-1);
+}
+
+close(fd);
+return (1);
+}
