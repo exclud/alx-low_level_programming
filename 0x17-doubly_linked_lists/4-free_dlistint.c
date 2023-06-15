fd0ad4a86ff11b364d6dd6e5c289f99d5b981f92
exclud <shammahnjomo12@gmail.com>
2023-06-15 22:28:00 +0300

    a function that frees a dlistint_t list.

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,14 @@
 #include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *current_node = head;
+    dlistint_t *next_node;
+
+    while (current_node != NULL)
+    {
+        next_node = current_node->next; /* Save the next node */
+        free(current_node); /* Free current node */
+        current_node = next_node; /* Move to the next node */
+    }
+}
