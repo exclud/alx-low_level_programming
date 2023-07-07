e7a9f9fe3fd291a22ed7aa87a838efca599ea9bf
exclud <shammahnjomo12@gmail.com>
2023-07-07 22:50:21 +0300

    A function that gives me the index of a key

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -25,8 +25,8 @@ hash_table_t *hash_table_create(unsigned long int size)
     hash_table->array = malloc(sizeof(hash_node_t *) * size);
     if (hash_table->array == NULL)
     {
-        free(hash_table);
-        return NULL;
+            free(hash_table);
+            return NULL;
     }
 
     for (i = 0; i < size; i++)
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,1 +1,22 @@
 #include "hash_tables.h"
+
+
+/**
+ * key_index - Gives the index of a key in the hash table array
+ * @key: The key
+ * @size: The size of the array
+ *
+ * Return: The index at which the key should be stored,
+ *         based on the hash_djb2 function
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int hash;
+    unsigned long int index;
+
+    hash = hash_djb2(key);
+
+    index = hash % size;
+
+    return index;
+}
