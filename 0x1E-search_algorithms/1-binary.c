a98fc2837d3849bcc5d07915dbd92e4f48edb042
exclud <shammahnjomo12@gmail.com>
2023-10-11 21:50:53 +0300

    Betty Formatting

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,26 +11,26 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t l = 0, r = size - 1, m;
+	size_t l = 0, r = size - 1, m;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    while (l <= r)
-    {
-        printf("Searching in array: ");
-        for (m = l; m < r; m++)
-            printf("%d, ", array[m]);
-        printf("%d\n", array[r]);
+	while (l <= r)
+	{
+		printf("Searching in array: ");
+		for (m = l; m < r; m++)
+			printf("%d, ", array[m]);
+		printf("%d\n", array[r]);
 
-        m = (l + r) / 2;
-        if (array[m] == value)
-            return (m);
-        else if (array[m] < value)
-            l = m + 1;
-        else
-            r = m - 1;
-    }
+		m = (l + r) / 2;
+		if (array[m] == value)
+			return (m);
+		else if (array[m] < value)
+			l = m + 1;
+		else
+			r = m - 1;
+	}
 
-    return (-1);
+	return (-1);
 }
